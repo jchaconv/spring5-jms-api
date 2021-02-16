d628fe513cd75c9575308a7f6d0aa6dc20580354
jchaconv <jcaaron.chacon@outlook.com>
2021-02-15 21:15:43 -0500

    Added HelloWorldMessage.java

---
+++ src/main/java/expert/springframework/spring5jmsapi/model/HelloWorldMessage.java
@@ -0,0 +1,22 @@
+package expert.springframework.spring5jmsapi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloWorldMessage implements Serializable {
+
+    static final long serialVersionUID = -9094681835194220139L;
+
+    private UUID id;
+    private String message;
+
+}
