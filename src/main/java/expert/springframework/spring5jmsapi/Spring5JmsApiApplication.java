1235e7c3b0a7a3da32493e64fb3a393f305f41e0
jchaconv <jcaaron.chacon@outlook.com>
2021-02-15 21:10:26 -0500

    Added initial files to the project.

---
+++ src/main/java/expert/springframework/spring5jmsapi/Spring5JmsApiApplication.java
@@ -0,0 +1,13 @@
+package expert.springframework.spring5jmsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5JmsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5JmsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/expert/springframework/spring5jmsapi/Spring5JmsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package expert.springframework.spring5jmsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5JmsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
